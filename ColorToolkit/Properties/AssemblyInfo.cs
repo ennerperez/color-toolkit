4f84539b869102d7821f6ffe02126229b7226bc4
Enner Pérez <ennerperez@gmail.com>
2016-03-10 00:40:28 -0430

    UI minors changes

--- ColorToolkit/Properties/AssemblyInfo.cs
+++ ColorToolkit/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Enner Pérez")]
 [assembly: AssemblyProduct("ColorToolkit")]
-[assembly: AssemblyCopyright("Copyright © Enner Pérez 2015")]
+[assembly: AssemblyCopyright("Copyright © Enner Pérez 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Resources;
 // Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
